4e686eaff058896669c65b5c96a1305f0197f9ee
Paul Davis <pjdavis@gmx.com>
2017-07-23 09:23:48 +0100

    modify test_url

---
+++ tests/test_metadata_url.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+from views.charts import get_metadata_url
+
+
+class TestGet_urls(TestCase):
+
+    def setUp(self):
+        self.expected_url = 'http://api.erg.kcl.ac.uk/AirQuality/Daily/MonitoringIndex/Latest/SiteCode=MY1/json'
+
+    def test_get_urls(self):
+        url = get_metadata_url('MY1')
+        self.assertEqual(url, self.expected_url)
