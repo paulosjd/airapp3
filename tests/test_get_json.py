e555cbaf136f1b75e8c58ed6ded2ab5ddc363f24
Paul Davis <pjdavis@gmx.com>
2017-07-27 11:37:13 +0100

    Update test_get_json.py

---
+++ tests/test_get_json.py
@@ -0,0 +1,20 @@
+import unittestimport json
+from views.charts 
+import get_json
+
+json_data = open('mock_data/my1_data.json')
+data = json.load(json_data)
+my1_array = data['AirQualityData']['Data']
+
+class TestCase(unittest.TestCase):        
+# Change values to allow for different dates    
+    def setUp(self):        
+        get_json_array = get_json('MY1', 3)        
+        for d in my1_array:            
+            for value in d:                
+            d[value] = '1'
+    
+    def test_dict_structure(self):                       
+        self.data1=get_json('MY1',3)[0].keys().replace('1')        
+        self.data2= my1_array[0].keys().replace('1')        
+        AssertEqual(self.get_json_array, self.my1_array)
