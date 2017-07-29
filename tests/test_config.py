b877aa5a27f9933f9895ce59d74c6562938af9a5
Paul Davis <pjdavis@gmx.com>
2017-07-29 10:28:43 +0100

    Update test_config.py

--- tests/test_config.py
+++ tests/test_config.py
@@ -5,7 +5,7 @@ from app import app
 class TestCase(unittest.TestCase):
     def setUp(self):
         tester = app.test_client(self)
-        self.response = tester.get('/chart/index')
+        self.response = tester.get('http://localhost:8080/chart/index')
 
     def test_sum(self):
         self.assertEqual(self.response.status_code, 200)
