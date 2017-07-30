6845a4d9332f19d577c884e03b960c87c260c93f
Paul Davis <pjdavis@gmx.com>
2017-07-30 08:43:14 +0100

    modify tests

--- tests/sc2.py
+++ tests/sc2.py
@@ -9,14 +9,21 @@ from views.charts import get_data
 get_data_dict = get_data('MY1', 3)
 
 
-length_dict = {key: len(value) for key, value in get_data_dict.items()}
-hours_len = length_dict['hours']
-no2_len = length_dict['no2']
-print(hours_len)
-print(no2_len)
+length_dict_1 = {key: len(value) for key, value in get_data_dict.items()}
+hours_len_1 = length_dict_1['hours']
+no2_len_1 = length_dict_1['no2']
+print(hours_len_1)
+print(no2_len_1)
 
 json_data2 = open('mock_data/my1_dict.json')
 my1_dict = json.load(json_data2)
+
+length_dict_2 = {key: len(value) for key, value in my1_dict.items()}
+hours_len_2 = length_dict_2['hours']
+no2_len_2 = length_dict_2['no2']
+print(hours_len_2)
+print(no2_len_2)
+
 
 """
 get_data_dict = get_data('MY1', 3)
