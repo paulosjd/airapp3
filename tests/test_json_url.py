ef952b97baa19256e8369c466012b8232be7207f
Paul Davis <pjdavis@gmx.com>
2017-07-30 09:28:30 +0100

    modify tests

--- tests/test_json_url.py
+++ tests/test_json_url.py
@@ -17,12 +17,6 @@ class TestGet_urls(TestCase):
         self.assertEqual(url.status_code, 200)
 
 
+if __name__ == '__main__':
+    unittest.main()
 
-
-s3 = 'http://api.erg.kcl.ac.uk/AirQuality/Data/Site/SiteCode=MY1/StartDate=11%11Jul%111111/' \
-                            'EndDate=11%11Jul%111111/Json'
-
-s2=(re.sub('[0-9]','1', s1))
-
-if s2==s3:
-    print(3)
